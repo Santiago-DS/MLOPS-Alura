a4504617138c07924de102900e9d1a3f8a93eea9
Gabriel Santiago <gabriel.santiago050299@gmail.com>
2024-02-28 09:17:47 -0300

    ajustes para encoding

--- src/app/main.py
+++ src/app/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify#  request irá servir para pegar o payload, jsonify vai servir para forncer os dados no formato json
+from flask import Flask, request, jsonify#  request ira servir para pegar o payload, jsonify vai servir para forncer os dados no formato json
 from flask_basicauth import BasicAuth
 from textblob import TextBlob
 from sklearn.linear_model import LinearRegression
@@ -6,13 +6,13 @@ import pickle
 import os
 
 # treinamento do modelo
-# so vai ser executado uma vez ao lançar o app
+# so vai ser executado uma vez ao lancar o app
 colunas = ['tamanho', 'ano', 'garagem'] # vamos usar variavel para garantir que as colunas sigam a ordem correta
 modelo = pickle.load(open('modelo.sav', 'rb'))
 
 
 app = Flask(__name__) # cria o app
-# é uma boa pratica passar a variável especial do python __name_
+# é uma boa pratica passar a variavel especial do python __name_
 # assim fica mais de saber onde essa aplicacao esta rodando
 # e facilita para o flask encontrar os recursos dessa aplicacao
 
@@ -22,12 +22,12 @@ app.config['BASIC_AUTH_PASSWORD'] = os.environ.get('BASIC_AUTH_PASSWORD')
 
 basic_autentication = BasicAuth(app) # precisamos colocar a autenticacao a cada endpoint
 
-@app.route('/') # diz qual rota o usuario deve usar para chegar nessa pagina. '/' indica que é a rota home
+@app.route('/') # diz qual rota o usuario deve usar para chegar nessa pagina. '/' indica que e a rota home
 def home(): # o que deve ser feito quando o usuario chegar nessa rota
     return "minha primeira API."
 
 
-# endpoint é a URL de acesso a nossa aplicação (ou pontos de acesso para nossa aplicação)
+# endpoint e a URL de acesso a nossa aplicacao (ou pontos de acesso para nossa aplicacao)
 # vamos criar um endpoint para nosso modelo
 @app.route('/sentimento/<frase>') 
 @basic_autentication.required # identificando que para esse endpoint, precisa de autenticacao
@@ -41,16 +41,16 @@ def sentimento(frase):
 
 
 # criando um novo endpoint para o outro modelo
-@app.route('/cotacao/', methods=['POST']) # por padrão é usado o GET (foi usado no sentimento), porém, como queremos receber vários dados, vamos usar o método POST
+@app.route('/cotacao/', methods=['POST']) # por padrao e usado o GET (foi usado no sentimento), porem, como queremos receber varios dados, vamos usar o metodo POST
 # esse conjunto de dados que vamos receber costuma ser chamado de payload
 def cotacao():
     
-    dados2 = request.get_json() # para pegar o json enviado pelo usuário
+    dados2 = request.get_json() # para pegar o json enviado pelo usuario
     # agora, vamos preparar esse arquivo json para o modelo, vamos colocar na ordem
     dados_input = [dados2[col] for col in colunas]
     preco = modelo.predict([dados_input])
-    # é interessante devolver o dado no formato jason também, já que o desenvolver pode trabalhar em cima do resultado do modelo
+    # é interessante devolver o dado no formato jason tambem, ja que o desenvolver pode trabalhar em cima do resultado do modelo
     # antes de servir ao usuário
     return jsonify(preco=preco[0])
-app.run(debug=True) # roda o aplicativo quando o código é executado
+app.run(debug=True) # roda o aplicativo quando o codigo e executado
 # quando o debug esta ON, o flask vai restartar a aplicacao automaticamente
